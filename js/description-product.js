780f32827cc3bfba235095065a403fb06cced438
Raphael Freire <raphaelmachadofreire@gmail.com>
2018-12-22 12:24:59 -0200

    finalizando segundo video da aula 15

---
+++ js/description-product.js
@@ -0,0 +1,8 @@
+const $heart = window.document.querySelector(".-heart");
+$heart.addEventListener("click", handleClick);
+
+function handleClick(){
+  $heart.classList.toggle('-active');
+ }
+
+
---
+++ js/home.js
@@ -0,0 +1,9 @@
+let ValorInicial = 10;
+
+const $second = window.document.querySelector(".-second");
+$second.addEventListener("click", handleClick);
+
+function handleClick(){
+  const $carrinho = document.querySelector(".-last");
+  $carrinho.textContent = `Carrinho (${++ValorInicial})`;
+}
