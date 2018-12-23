cd6a1205e2b6b60e80b02ed889070fe4689d120e
Raphael Freire <raphaelmachadofreire@gmail.com>
2018-12-23 14:31:45 -0200

    estrela implementada utilizando lista

--- js/description-product.js
+++ js/description-product.js
@@ -1,8 +1,18 @@
 const $heart = window.document.querySelector(".-heart");
-$heart.addEventListener("click", handleClick);
+const $stars = window.document.querySelectorAll(".star");
+let index = 0;
 
-function handleClick(){
-  $heart.classList.toggle('-active');
+$heart.addEventListener("click", handleClickHeart);
+function handleClickHeart(){
+  $heart.classList.toggle("-active");
  }
+
+$stars.forEach(function($star){
+  $star.addEventListener("click", handleStar);
+});
+
+function handleStar(){
+  this.classList.toggle("-active");
+}
 
 
