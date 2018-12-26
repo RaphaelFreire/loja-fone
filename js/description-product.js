886f85639cff9969c93843c2e6cd6a5181f56f03
Raphael Freire <raphaelmachadofreire@gmail.com>
2018-12-25 22:34:52 -0200

    Desafio aula 16 feito

--- js/description-product.js
+++ js/description-product.js
@@ -1,18 +1,56 @@
 const $heart = window.document.querySelector(".-heart");
 const $stars = window.document.querySelectorAll(".star");
 let index = 0;
+const positionLast = $stars.length - 1;
 
-$heart.addEventListener("click", handleClickHeart);
-function handleClickHeart(){
-  $heart.classList.toggle("-active");
+$heart.addEventListener("click", handleClick);
+function handleClick(){
+  this.classList.toggle("-active");
  }
 
-$stars.forEach(function($star){
-  $star.addEventListener("click", handleStar);
-});
-
-function handleStar(){
+ function handleClick(){
   this.classList.toggle("-active");
 }
+
+$stars.forEach(function($star, key){
+  if(key == 0){
+     $star.addEventListener("click", firstStar);
+  }
+
+  if(key>0 && key < positionLast){
+        $star.addEventListener("click", function(){
+          middleStar(key);
+        });
+  }
+
+  if(key == positionLast){
+     $star.addEventListener("click", lastStar);
+  }
+});
+
+function firstStar(){
+  $stars.forEach(function($star){
+    $star.classList.remove("-active");
+  });
+  this.classList.add("-active");
+}
+
+function middleStar(index){
+  $stars.forEach(function($star, key){
+    $star.classList.remove("-active");
+  
+  if(key <= index){
+      $star.classList.add("-active");
+    } 
+  });
+ }
+
+function lastStar(){
+  $stars.forEach(function($star){
+    $star.classList.add("-active");
+  });
+}
+
+
 
 
